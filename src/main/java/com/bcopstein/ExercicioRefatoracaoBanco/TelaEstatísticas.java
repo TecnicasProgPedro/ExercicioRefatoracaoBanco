1de47ffb6880271f975eec8fad60b5f5f57db562
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-13 02:01:19 -0200

    Update src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
    Saldo medio mes!!! Sem interface por enquanto.

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
@@ -1,5 +1,36 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
+import java.util.List;
+
 public class TelaEstatísticas {
+	private List<Operacao> operacoes;
+	private Conta conta;
+
+	public double SaldoMedioMes(int mes, int ano) {
+		double Soma = 0;
+		for (Operacao op : operacoes) {
+			if (conta.getNumero() == op.getNumeroConta()) {
+				if (op.getAno() == ano) {
+					if (op.getMes() > mes) {
+						if (op.getTipoOperacao() == 1) {
+							Soma-=op.getValorOperacao();
+						}
+						else {
+							Soma+=op.getValorOperacao();
+						}
+					}
+				}
+				else if(op.getAno() > ano) {
+					if (op.getTipoOperacao() == 1) {
+						Soma-=op.getValorOperacao();
+					}
+					else {
+						Soma+=op.getValorOperacao();
+					}
+				}
+			}
+		}
+		return (conta.getSaldo() - (Soma))/30;
+	}
 
 }
