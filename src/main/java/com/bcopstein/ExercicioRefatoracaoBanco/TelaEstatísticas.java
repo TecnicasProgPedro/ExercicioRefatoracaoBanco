795eedea6a8114dd9c6d9ea8a4c9f71ec11e957e
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-25 20:19:08 -0200

    Update src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
@@ -1,11 +1,8 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
-<<<<<<< HEAD
 import java.io.IOException;
 import java.util.ArrayList;
-=======
 
->>>>>>> 28f9169011bf769bee3668826de9963164511912
 import java.util.List;
 import java.util.Observable;
 
@@ -25,52 +22,47 @@ public class TelaEstatísticas {
 	private List<Operacao> operacoes;
 	private Conta conta;
 	private Stage mainStage;
-<<<<<<< HEAD
+
 	private Scene telaOP;
-	
-	public TelaEstatísticas(List<Operacao> operacoes,Conta conta,Stage mainStage, Scene telaOP) throws IOException{
-		this.operacoes=operacoes;
-		this.conta=conta;
-		this.mainStage=mainStage;
-		this.telaOP=telaOP;
+
+	public TelaEstatísticas(List<Operacao> operacoes, Conta conta, Stage mainStage, Scene telaOP) throws IOException {
+		this.operacoes = operacoes;
+		this.conta = conta;
+		this.mainStage = mainStage;
+		this.telaOP = telaOP;
 	}
-	
+
 	public Scene getEstatisticas() throws IOException {
-		Pane root=FXMLLoader.load(getClass().getResource("FXMLtelaEstats.fxml"));
-		Scene sceneEstats=new Scene(root,300,200);
-		
-	
+		Pane root = FXMLLoader.load(getClass().getResource("FXMLtelaEstats.fxml"));
+		Scene sceneEstats = new Scene(root, 300, 200);
+
 		return sceneEstats;
 	}
-	
-	//-------------------------------------
-=======
-	private Stage cenaEstats;
-	
-	public TelaEstatísticas(Conta conta, List<Operacao> operacoes) {
-		this.conta=conta;
-		this.operacoes=operacoes;
-	}
-	
-	public Scene getTelaEstatísticas(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Welcome");
-        
-        primaryStage.show();
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-        
-        return scene;
-    }
-	
-	
-	
->>>>>>> 28f9169011bf769bee3668826de9963164511912
+	// -------------------------------------
+	private Stage cenaEstats;
+
+	public TelaEstatísticas(Conta conta, List<Operacao> operacoes) {
+		this.conta = conta;
+		this.operacoes = operacoes;
+	}
+
+	public Scene getTelaEstatísticas(Stage primaryStage) {
+		primaryStage.setTitle("JavaFX Welcome");
+
+		primaryStage.show();
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+
+		Scene scene = new Scene(grid, 300, 275);
+		primaryStage.setScene(scene);
+
+		return scene;
+	}
+
 	public double SaldoMedioMes(int mes, int ano) {
 		double Soma = 0;
 		for (Operacao op : operacoes) {
@@ -78,46 +70,46 @@ public class TelaEstatísticas {
 				if (op.getAno() == ano) {
 					if (op.getMes() > mes) {
 						if (op.getTipoOperacao() == 1) {
-							Soma-=op.getValorOperacao();
-						}
-						else {
-							Soma+=op.getValorOperacao();
+							Soma -= op.getValorOperacao();
+						} else {
+							Soma += op.getValorOperacao();
 						}
 					}
-				}
-				else if(op.getAno() > ano) {
+				} else if (op.getAno() > ano) {
 					if (op.getTipoOperacao() == 1) {
-						Soma-=op.getValorOperacao();
-					}
-					else {
-						Soma+=op.getValorOperacao();
+						Soma -= op.getValorOperacao();
+					} else {
+						Soma += op.getValorOperacao();
 					}
 				}
 			}
 		}
-		return (conta.getSaldo() - (Soma))/30;
+		return (conta.getSaldo() - (Soma)) / 30;
 	}
+
 	public ArrayList<Integer> listaMes() {
-		ArrayList<Integer> listaMes=new ArrayList();
-		for(Operacao op: operacoes) {
-			if(ok(listaMes,op.getMes())==true) {
+		ArrayList<Integer> listaMes = new ArrayList();
+		for (Operacao op : operacoes) {
+			if (ok(listaMes, op.getMes()) == true) {
 				listaMes.add(op.getMes());
 			}
 		}
 		return listaMes;
 	}
+
 	public ArrayList<Integer> listaAno() {
-		ArrayList<Integer> listaAno=new ArrayList();
-		for(Operacao op: operacoes) {
-			if(ok(listaAno,op.getAno())==true) {
+		ArrayList<Integer> listaAno = new ArrayList();
+		for (Operacao op : operacoes) {
+			if (ok(listaAno, op.getAno()) == true) {
 				listaAno.add(op.getAno());
 			}
 		}
 		return listaAno;
 	}
-	private boolean ok(ArrayList<Integer> lista,int num) {
-		for(int i:lista) {
-			if(i==num) {
+
+	private boolean ok(ArrayList<Integer> lista, int num) {
+		for (int i : lista) {
+			if (i == num) {
 				return false;
 			}
 		}
