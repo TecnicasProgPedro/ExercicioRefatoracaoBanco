7f5baf97693428bdd74b27db19e640dba7576e8f
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-25 18:58:54 -0200

    Update src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatísticas.java
@@ -1,5 +1,6 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TelaEstatísticas {
@@ -32,5 +33,30 @@ public class TelaEstatísticas {
 		}
 		return (conta.getSaldo() - (Soma))/30;
 	}
-
+	public ArrayList<Integer> listaMes() {
+		ArrayList<Integer> listaMes=new ArrayList();
+		for(Operacao op: operacoes) {
+			if(ok(listaMes,op.getMes())==true) {
+				listaMes.add(op.getMes());
+			}
+		}
+		return listaMes;
+	}
+	public ArrayList<Integer> listaAno() {
+		ArrayList<Integer> listaAno=new ArrayList();
+		for(Operacao op: operacoes) {
+			if(ok(listaAno,op.getAno())==true) {
+				listaAno.add(op.getAno());
+			}
+		}
+		return listaAno;
+	}
+	private boolean ok(ArrayList<Integer> lista,int num) {
+		for(int i:lista) {
+			if(i==num) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
