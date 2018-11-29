8161103e08c6591fa159b89105c32a2a908830d5
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-29 02:04:34 -0200

    Update Operacoes.java
    Singleton

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
@@ -1,5 +1,4 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
@@ -13,67 +12,70 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Operacoes {
+	private List<Operacao> operacoes;
 	private final String NomeBDOperacoes = "BDOperBNG.txt";
+	private static Operacoes INSTANCE;
 	
-	public Operacoes() {}
+	public static Operacoes getInstance() {
+		 if (INSTANCE == null) {
+				INSTANCE = new Operacoes();
+			}
+		 return INSTANCE;
+	}
 	
+	private Operacoes() {
+		this.operacoes=loadOperacoes();
+	}
+	public List<Operacao> getOperacoes(){
+		return this.operacoes;
+	}
+	
+	private List<Operacao> loadOperacoes() {
+		List<Operacao> operacoes = new LinkedList<Operacao>();
+
+		String currDir = Paths.get("").toAbsolutePath().toString();
+		String nameComplete = currDir + "\\" + NomeBDOperacoes;
+		System.out.println(nameComplete);
+		Path path2 = Paths.get(nameComplete);
+		try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.defaultCharset()))) {
+			sc.useDelimiter("[;\n]"); // separadores: ; e nova linha
+			int dia, mes, ano;
+			int hora, minuto, segundo;
+			int numero, status, tipo;
+			double valor;
+
+			while (sc.hasNext()) {
+				dia = Integer.parseInt(sc.next());
+				mes = Integer.parseInt(sc.next());
+				ano = Integer.parseInt(sc.next());
+				hora = Integer.parseInt(sc.next());
+				minuto = Integer.parseInt(sc.next());
+				segundo = Integer.parseInt(sc.next());
+				numero = Integer.parseInt(sc.next());
+				status = Integer.parseInt(sc.next());
+				valor = Double.parseDouble(sc.next());
+				tipo = Integer.parseInt(sc.next());
+
+				Operacao op = new Operacao(dia, mes, ano, hora, minuto, segundo, numero, status, valor, tipo);
+
+				operacoes.add(op);
+			}
+		} catch (IOException x) {
+			System.err.format("Erro de E/S: %s%n", x);
+			return null;
+		}
+		return operacoes;
+	}
+
 	public void saveOperacoes(Collection<Operacao> operacoes) {
-        Path path1 = Paths.get(NomeBDOperacoes); 
-        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.defaultCharset()))) 
-        { 
-            for(Operacao op:operacoes) 
-                writer.format(Locale.ENGLISH,
-                		      "%d;%d;%d;%d;%d;%d;%d;%d;%f;%d;",  
-                              op.getDia(),op.getMes(),op.getAno(),
-                              op.getHora(),op.getMinuto(),op.getSegundo(),
-                              op.getNumeroConta(),op.getStatusConta(),
-                              op.getValorOperacao(),op.getTipoOperacao()
-                             ); 
-        } 
-        catch (IOException x) 
-        { 
-            System.err.format("Erro de E/S: %s%n", x); 
-        } 
-    }
-    
-    public List<Operacao> loadOperacoes(){
-        List<Operacao> operacoes = new LinkedList<Operacao>();
-        
-    	String currDir = Paths.get("").toAbsolutePath().toString();
-        String nameComplete = currDir+"\\"+NomeBDOperacoes;
-        System.out.println(nameComplete);
-        Path path2 = Paths.get(nameComplete); 
-        try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.defaultCharset()))){ 
-           sc.useDelimiter("[;\n]"); // separadores: ; e nova linha 
-           int dia,mes,ano;
-           int hora,minuto,segundo;
-           int numero,status,tipo;
-           double valor;
-       
-           while (sc.hasNext()){ 
-               dia = Integer.parseInt(sc.next()); 
-               mes = Integer.parseInt(sc.next()); 
-               ano = Integer.parseInt(sc.next()); 
-               hora = Integer.parseInt(sc.next()); 
-               minuto = Integer.parseInt(sc.next()); 
-               segundo = Integer.parseInt(sc.next()); 
-               numero = Integer.parseInt(sc.next()); 
-               status = Integer.parseInt(sc.next()); 
-               valor = Double.parseDouble(sc.next());
-               tipo = Integer.parseInt(sc.next());
-               
-               Operacao op = new Operacao(
-            		   dia, mes, ano,
-            		   hora, minuto, segundo,
-	                   numero, status,
-	                   valor, tipo);
-               
-               operacoes.add(op);
-           }
-        }catch (IOException x){ 
-            System.err.format("Erro de E/S: %s%n", x);
-            return null;
-        } 
-        return operacoes;    	
-    }
+		Path path1 = Paths.get(NomeBDOperacoes);
+		try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.defaultCharset()))) {
+			for (Operacao op : operacoes)
+				writer.format(Locale.ENGLISH, "%d;%d;%d;%d;%d;%d;%d;%d;%f;%d;", op.getDia(), op.getMes(), op.getAno(),
+						op.getHora(), op.getMinuto(), op.getSegundo(), op.getNumeroConta(), op.getStatusConta(),
+						op.getValorOperacao(), op.getTipoOperacao());
+		} catch (IOException x) {
+			System.err.format("Erro de E/S: %s%n", x);
+		}
+	}
 }
