581e4e8f922ab8ed7151d9ffaa881b2755d2fca4
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-29 05:23:17 -0200

    Update LogicaOperacoes.java

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
@@ -1,9 +1,14 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
+import java.util.ArrayList;
+
 public class LogicaOperacoes {
 	private static LogicaOperacoes INSTANCE;
+	private Persistencia persistencia;
 	
-	private LogicaOperacoes() {}
+	private LogicaOperacoes() {
+		this.persistencia=Persistencia.getInstance();
+	}
 	
 	public static LogicaOperacoes getInstance() {
 		if (INSTANCE == null) {
@@ -12,11 +17,28 @@ public class LogicaOperacoes {
 		return INSTANCE;
 	}
 	
-	public void DefinirContaEmUso(){}
-	public void OperaçaoDeCredito(){}
-	public void OperacaoDeDebito(){}
-	public void SolicitaExtrato(){}
-	public void SolicitaSaldo(){}
-	public void SolicitaSaldoMedio(){}
-	public void TotalCreditos(){}
+	public Conta DefinirContaEmUso(int nroConta){
+		return persistencia.getContas().contaEmUso(nroConta);
+	}
+	public void OperaçaoDeCredito(Conta c,double valor){
+		c.deposito(valor);
+	}
+	public void OperacaoDeDebito(Conta c,double valor){
+		c.retirada(valor);
+	}
+	public double SolicitaSaldoMedio(int mes,int ano,Conta conta){
+		return persistencia.getOperacoes().SaldoMedioMes(mes, ano, conta);
+	}
+	public double creditoMes(int mes, int ano, Conta conta) {
+		return persistencia.getOperacoes().creditosMes(mes, ano, conta);
+	}
+	public double debitoMes(int mes, int ano,Conta conta) {
+		return persistencia.getOperacoes().debitosMes(mes, ano, conta);
+	}
+	public ArrayList<Integer> listMes(){
+		return persistencia.getOperacoes().listaMes();
+	}
+	public ArrayList<Integer> listAno(){
+		return persistencia.getOperacoes().listaAno();
+	}
 }
