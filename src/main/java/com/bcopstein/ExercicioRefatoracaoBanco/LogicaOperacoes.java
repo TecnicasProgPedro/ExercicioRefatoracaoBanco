2fb1a83692610f3ad2b9b9b4a5894a15c7cf9f73
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-29 03:28:14 -0200

    Update LogicaOperacoes.java

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
@@ -1,5 +1,22 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
 public class LogicaOperacoes {
-
+	private static LogicaOperacoes INSTANCE;
+	
+	private LogicaOperacoes() {}
+	
+	public static LogicaOperacoes getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new LogicaOperacoes();
+		}
+		return INSTANCE;
+	}
+	
+	public void DefinirContaEmUso(){}
+	public void OperaçaoDeCredito(){}
+	public void OperacaoDeDebito(){}
+	public void SolicitaExtrato(){}
+	public void SolicitaSaldo(){}
+	public void SolicitaSaldoMedio(){}
+	public void TotalCreditos(){}
 }
