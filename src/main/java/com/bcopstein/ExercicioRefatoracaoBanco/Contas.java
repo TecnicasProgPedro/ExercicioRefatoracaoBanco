242c7b2730b9bbfbf83ee5e3ce8c4ab475eb316a
Pedrowx <pedrowx1@users.noreply.github.com>
2018-11-29 02:04:31 -0200

    Update Contas.java
    Singleton

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Contas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Contas.java
@@ -1,5 +1,4 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
@@ -13,51 +12,62 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Contas {
+	 private  Map<Integer,Conta> contas;
 	 private final String NomeBDContas = "BDContasBNG.txt";
+	 private static Contas INSTANCE;
 	 
-	 public Contas() {}
+	 public static Contas getInstance() {
+		 if (INSTANCE == null) {
+				INSTANCE = new Contas();
+			}
+		 return INSTANCE;
+	 }
 	 
-	 public Map<Integer,Conta> loadContas(){
-	    	Map<Integer,Conta> contas = new HashMap<>();
-	    	
-	        String currDir = Paths.get("").toAbsolutePath().toString();
-	        String nameComplete = currDir+"\\"+NomeBDContas;
-	        System.out.println(nameComplete);
-	        Path path2 = Paths.get(nameComplete); 
-	        try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.defaultCharset()))){ 
-	           sc.useDelimiter("[;\n]"); // separadores: ; e nova linha 
-	           int numero;
-	           String nomeCorr;
-	           double saldo;
-	           int status;
-	           while (sc.hasNext()){ 
-	               numero = Integer.parseInt(sc.next()); 
-	               nomeCorr = sc.next();
-	               saldo = Double.parseDouble(sc.next());
-	               status = Integer.parseInt(sc.next());
-	               Conta conta = new Conta(numero,nomeCorr,saldo,status);
-	               contas.put(numero, conta);
-	           }
-	        }catch (IOException x){ 
-	            System.err.format("Erro de E/S: %s%n", x);
-	            return null;
-	        } 
-	        return contas;
-	    }
+	 private Contas() {
+		 this.contas=loadContas();
+	 }
+	 
+	 public Map<Integer,Conta> getContas(){
+		 return this.contas;
+	 }
+	 
+	 private Map<Integer, Conta> loadContas() {
+			Map<Integer, Conta> contas = new HashMap<>();
 
-	    public void saveContas(Collection<Conta> contas) {
-	        Path path1 = Paths.get(NomeBDContas); 
-	        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.defaultCharset()))) 
-	        { 
-	            for(Conta c: contas) 
-	                writer.format(Locale.ENGLISH,
-	                		      "%d;%s;%f;%d;",
-	                		      c.getNumero(),c.getCorrentista(), 
-	                              c.getSaldo(),c.getStatus()); 
-	        } 
-	        catch (IOException x) 
-	        { 
-	            System.err.format("Erro de E/S: %s%n", x); 
-	        } 
-	    }
+			String currDir = Paths.get("").toAbsolutePath().toString();
+			String nameComplete = currDir + "\\" + NomeBDContas;
+			System.out.println(nameComplete);
+			Path path2 = Paths.get(nameComplete);
+			try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.defaultCharset()))) {
+				sc.useDelimiter("[;\n]"); // separadores: ; e nova linha
+				int numero;
+				String nomeCorr;
+				double saldo;
+				int status;
+				while (sc.hasNext()) {
+					numero = Integer.parseInt(sc.next());
+					nomeCorr = sc.next();
+					saldo = Double.parseDouble(sc.next());
+					status = Integer.parseInt(sc.next());
+					Conta conta = new Conta(numero, nomeCorr, saldo, status);
+					contas.put(numero, conta);
+				}
+			} catch (IOException x) {
+				System.err.format("Erro de E/S: %s%n", x);
+				return null;
+			}
+			return contas;
+		}
+
+		public void saveContas(Collection<Conta> contas) {
+			Path path1 = Paths.get(NomeBDContas);
+			try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path1, Charset.defaultCharset()))) {
+				for (Conta c : contas)
+					writer.format(Locale.ENGLISH, "%d;%s;%f;%d;", c.getNumero(), c.getCorrentista(), c.getSaldo(),
+							c.getStatus());
+			} catch (IOException x) {
+				System.err.format("Erro de E/S: %s%n", x);
+			}
+		}
+	 
 }
